773ed2f5a27c07a7f99a23d0a53061f688e823ba
fahad4920 <fahadsajid492@gmail.com>
2025-12-03 14:56:27 +0500

    updated

--- server/controllers/ChatController.js
+++ server/controllers/ChatController.js
@@ -11,25 +11,24 @@ export const createChat = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
 export const userChats = async (req, res) => {
   try {
     const chat = await ChatModel.find({
       members: { $in: [req.params.userId] },
     });
-    
     res.status(200).json(chat);
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
 export const findChat = async (req, res) => {
   try {
     const chat = await ChatModel.findOne({
       members: { $all: [req.params.firstId, req.params.secondId] },
     });
-
     res.status(200).json(chat)
-
   } catch (error) {
     res.status(500).json(error)
   }
